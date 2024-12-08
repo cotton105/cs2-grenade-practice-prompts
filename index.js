248e3fa272e99f6111d46f598d086bbc931dbf00
Luke Cotton <luke@cotton.org.uk>
2024-12-08 13:04:11 +0000

    Add countdown for next grenade prompt

--- index.js
+++ index.js
@@ -11,7 +11,20 @@ setInterval(chooseRandomGrenade, config.intervalS * 1000, targetGrenades);
 
 function chooseRandomGrenade(targetGrenades) {
   const randomIndex = Math.floor(Math.random() * targetGrenades.length);
+  const nextGrenadeWarningS = (config.intervalS - 5) - 1;
   displayNewPrompt(targetGrenades, randomIndex);
+  
+  setTimeout(function () {
+    let countdownS = 5;
+    let displayCountdown;
+    displayCountdown = setInterval(function () {
+      console.log(`Advancing in ${countdownS} seconds...`);
+      countdownS--;
+      if (countdownS < 1) {
+        clearInterval(displayCountdown);
+      }
+    }, 1000);
+  }, nextGrenadeWarningS * 1000)
 }
 
 function displayNewPrompt(grenadeList, index) {
